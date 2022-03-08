4767752a926aa6dd679b0e41a24171d25a800e2c
Guilherme Vincenzi Cavichioli <guilhermecavichioli@hotmail.com>
2022-03-08 20:42:04 -0300

    chore: add babel and react

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,21 @@ module.exports = {
   devServer: {
     port: 3000
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime']
+          }
+        }
+      }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html'
