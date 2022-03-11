411bb9a202e559d52c0540dccf82e8678378cb83
Guilherme Vincenzi Cavichioli <guilhermecavichioli@hotmail.com>
2022-03-11 02:56:23 -0300

    feat: improve styles and create ds styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import ReactDOM from "react-dom";
 
 import { makeApp } from "./main/makers/make-app";
 
+import "./styles/global.scss";
+
 ReactDOM.render(
   makeApp(),
   document.getElementById("root")
