d4c551a0032049ae0467839786434b16d11751b8
Guilherme Vincenzi Cavichioli <guilhermecavichioli@hotmail.com>
2022-03-11 11:52:56 -0300

    refactor: add getimagesbytype to the search card component

--- src/components/SearchCard/SearchCard.jsx
+++ src/components/SearchCard/SearchCard.jsx
@@ -3,17 +3,33 @@ import React from "react";
 
 import styles from "./SearchCard.module.scss";
 
-const SearchCard = ({ searchItem, images }) => (
-  <div className={styles.SearchCard} key={searchItem.id}>
-    <div className={styles.Cover}>
-      <img src={images[0].url} alt={`${searchItem.type} name ${searchItem.name}`} />
+const getImagesByType = (item) => {
+  if (item.type === "album") {
+    return item.images;
+  }
+
+  if (item.type === "track") {
+    return item.album.images;
+  }
+
+  return [];
+};
+
+const SearchCard = ({ searchItem }) => {
+  const images = getImagesByType(searchItem);
+
+  return (
+    <div className={styles.SearchCard} key={searchItem.id}>
+      <div className={styles.Cover}>
+        <img src={images[0]?.url} alt={`${searchItem.type} name ${searchItem.name}`} />
+      </div>
+      <div className={styles.Info}>
+        <span className={styles.Name}>{searchItem.name}</span>
+        <span className={styles.ArtistName}>{searchItem.artists[0].name}</span>
+      </div>
     </div>
-    <div className={styles.Info}>
-      <span className={styles.Name}>{searchItem.name}</span>
-      <span className={styles.ArtistName}>{searchItem.artists[0].name}</span>
-    </div>
-  </div>
-);
+  );
+};
 
 SearchCard.propTypes = {
   searchItem: PropTypes.shape({
@@ -23,10 +39,10 @@ SearchCard.propTypes = {
       PropTypes.shape({ name: PropTypes.string.isRequired })
     ).isRequired,
     type: PropTypes.string.isRequired,
+    images: PropTypes.arrayOf(
+      PropTypes.shape({ url: PropTypes.string.isRequired })
+    ).isRequired,
   }).isRequired,
-  images: PropTypes.arrayOf(
-    PropTypes.shape({ url: PropTypes.string.isRequired })
-  ).isRequired,
 };
 
 export default SearchCard;
