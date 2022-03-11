85860f24e729ecc6cb000207ffc51d424434e872
Guilherme Vincenzi Cavichioli <guilhermecavichioli@hotmail.com>
2022-03-11 18:24:31 -0300

    feat: export all components from components folder

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,7 @@
+export * from "./Header";
 export * from "./HorizontalList";
+export * from "./Layout";
+export * from "./Logo";
 export * from "./SearchCard";
 export * from "./SearchInput";
+export * from "./TracksContainer";
