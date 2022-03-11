c8489c45b14de90344ddf40a3224c89fe9e546c2
Guilherme Vincenzi Cavichioli <guilhermecavichioli@hotmail.com>
2022-03-11 11:55:02 -0300

    feat: add home screen with search and list for tracks and albums

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,52 @@
-import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 
-import { useToken } from "../redux/slices/Auth";
+import {
+  HorizontalList, SearchCard, SearchInput,
+} from "../components";
+import { useDebounce } from "../hooks/useDebounce";
+import {
+  clearSearch,
+  searchByQueryString, useAlbums, useTracks,
+} from "../redux/slices/Search";
+
+import styles from "./styles/Home.module.scss";
 
 const Home = () => {
-  const token = useToken();
+  const dispatch = useDispatch();
+  const [queryString, setQueryString] = useState("");
+  const debounceQueryString = useDebounce(queryString, 500);
+
+  const albums = useAlbums();
+  const tracks = useTracks();
+
+  useEffect(() => {
+    if (debounceQueryString.length > 0) {
+      dispatch(searchByQueryString(debounceQueryString));
+    } else {
+      dispatch(clearSearch());
+    }
+  }, [debounceQueryString]);
+
+  const resultsTest = debounceQueryString.length > 0
+    ? `Resultados encontrados para "${debounceQueryString}"`
+    : "Álbuns buscados recentemente";
 
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.Container}>
+      <main className={styles.MainContent}>
+        <SearchInput value={queryString} onChange={setQueryString} />
+
+        <h3 className="Text heading">{resultsTest}</h3>
+
+        <HorizontalList>
+          {
+            albums?.items.concat(tracks?.items).map((album) => (
+              <SearchCard searchItem={album} />
+            ))
+          }
+        </HorizontalList>
+      </main>
     </div>
   );
 };
