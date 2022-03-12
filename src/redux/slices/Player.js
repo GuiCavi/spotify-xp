391c3733653534308b450d50a7fd5b50bc12bae7
Guilherme Vincenzi Cavichioli <guilhermecavichioli@hotmail.com>
2022-03-12 15:39:51 -0300

    feat: add slices for controlling player

---
+++ src/redux/slices/Player.js
@@ -0,0 +1,43 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { useSelector } from "react-redux";
+
+const PlayerSlice = createSlice({
+  name: "player",
+  initialState: {
+    playlist: [],
+    currentSong: null,
+    currentSongIndex: 0,
+    isPlaying: false,
+  },
+  reducers: {
+    setPlaylist: (state, action) => {
+      state.playlist = action.payload;
+      state.currentSong = action.payload[0];
+      state.isPlaying = true;
+    },
+    pausePlaylist: (state) => {
+      state.isPlaying = false;
+    },
+    playPlaylist: (state) => {
+      state.isPlaying = true;
+    },
+    nextSong: (state) => {
+      state.currentSongIndex += 1;
+      state.currentSong = state.playlist[state.currentSongIndex];
+    },
+    prevSong: (state) => {
+      state.currentSongIndex -= 1;
+      state.currentSong = state.playlist[state.currentSongIndex];
+    },
+  },
+});
+
+export const {
+  setPlaylist, pausePlaylist, playPlaylist, nextSong, prevSong,
+} = PlayerSlice.actions;
+
+export default PlayerSlice.reducer;
+
+export const useCurrentSong = () => useSelector((state) => state.player.currentSong);
+export const useIsPlaying = () => useSelector((state) => state.player.isPlaying);
+export const useHasPlayer = () => useSelector((state) => !!state.player.currentSong);
