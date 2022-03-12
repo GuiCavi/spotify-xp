31fb45a1bbe9457494e44d07f5a98b82f3cd793d
Guilherme Vincenzi Cavichioli <guilhermecavichioli@hotmail.com>
2022-03-12 17:31:44 -0300

    feat: improve tracks container to give playing feedback

--- src/components/TracksContainer/TracksContainer.jsx
+++ src/components/TracksContainer/TracksContainer.jsx
@@ -1,12 +1,13 @@
 import PropTypes from "prop-types";
 
 import { Duration } from "../../data/usecases/Duration";
+import { useCurrentSong } from "../../redux/slices/Player";
 import { useSelectedAlbumTracks } from "../../redux/slices/Search";
 
 import styles from "./TracksContainer.module.scss";
 
-const TracksContainer = () => {
-  const tracks = useSelectedAlbumTracks();
+const TracksContainer = ({ tracks }) => {
+  const currentSong = useCurrentSong();
 
   if (!tracks) {
     return <span>Carregando...</span>;
@@ -16,19 +17,41 @@ const TracksContainer = () => {
     <ul className={styles.AlbumTracks}>
       {
         tracks.items.map((track) => (
-          <TrackItem key={track.id} track={track} />
+          <TrackItem key={track.id} track={track} selected={currentSong?.id === track.id} />
         ))
       }
     </ul>
   );
 };
 
-const TrackItem = ({ track }) => (
-  <li>
-    <span className={styles.TrackName}>{track.name}</span>
-    <span className={styles.TrackDuration}>{Duration.format(track.duration_ms)}</span>
-  </li>
-);
+TracksContainer.propTypes = {
+  tracks: PropTypes.shape({
+    items: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        duration_ms: PropTypes.number.isRequired,
+        artists: PropTypes.arrayOf(
+          PropTypes.shape({ name: PropTypes.string.isRequired })
+        ).isRequired,
+      })
+    ).isRequired,
+  }).isRequired,
+};
+
+const TrackItem = ({ track, selected }) => {
+  const classname = [styles.TrackItem];
+  if (selected) {
+    classname.push(styles.TrackItemSelected);
+  }
+
+  return (
+    <li className={classname.join(" ")}>
+      <span className={styles.TrackName}>{track.name}</span>
+      <span className={styles.TrackDuration}>{Duration.format(track.duration_ms)}</span>
+    </li>
+  );
+};
 
 TrackItem.propTypes = {
   track: PropTypes.shape({
@@ -36,6 +59,9 @@ TrackItem.propTypes = {
     name: PropTypes.string.isRequired,
     duration_ms: PropTypes.number.isRequired,
   }).isRequired,
+  selected: PropTypes.bool,
 };
+
+TrackItem.defaultProps = { selected: false };
 
 export default TracksContainer;
